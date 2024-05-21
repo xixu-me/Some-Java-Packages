4c828b307ee33444c13ff5302492d26e8e22c0ee
Xi Xu <i@xi-xu.me>
2024-05-21 11:49:54 +0800

    Update

--- finalexam/StandardIO/Main.java
+++ finalexam/StandardIO/Main.java
@@ -1,3 +1,5 @@
+package finalexam.StandardIO;
+
 import java.util.Scanner;
 
 public class Main {
--- finalexam/StandardIO/PrimeNumbers.java
+++ finalexam/StandardIO/PrimeNumbers.java
@@ -1,3 +1,5 @@
+package finalexam.StandardIO;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;

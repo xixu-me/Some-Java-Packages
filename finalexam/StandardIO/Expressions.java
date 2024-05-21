4b2421527920086d613d207c716d5ccd63169651
Xi Xu <i@xi-xu.me>
2024-05-21 11:24:36 +0800

    Update

--- finalexam/StandardIO/Expressions.java
+++ finalexam/StandardIO/Expressions.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SimpleExpressions {
+public class Expressions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
---
+++ finalexam/StandardIO/SecurePasswordSpecification.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SecurePasswordSpecification {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(isSafePassword(s) ? "success" : "fail");
+    }
+
+    private static boolean isSafePassword(String password) {
+        if (password.length() < 6 || password.length() > 12)
+            return false;
+        boolean haveLower = false, hasUpper = false, haveDigit = false, hasWhitespace = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c))
+                haveLower = true;
+            else if (Character.isUpperCase(c))
+                hasUpper = true;
+            else if (Character.isDigit(c))
+                haveDigit = true;
+            else if (Character.isWhitespace(c))
+                hasWhitespace = true;
+        }
+        return haveLower && hasUpper && haveDigit && !hasWhitespace;
+    }
+}

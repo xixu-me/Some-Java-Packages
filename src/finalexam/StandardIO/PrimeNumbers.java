d42322b89fa03294db413f7422367f5094031eda
Xi Xu <i@xi-xu.me>
2024-05-24 21:45:25 +0800

    Update PrimeNumbers.java

--- src/finalexam/StandardIO/PrimeNumbers.java
+++ src/finalexam/StandardIO/PrimeNumbers.java
@@ -1,36 +1,33 @@
 package finalexam.StandardIO;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class PrimeNumbers {
-    private static boolean isPrime(int n) {
-        if (n <= 1)
-            return false;
-        for (int i = 2; i <= Math.sqrt(n); i++)
-            if (n % i == 0)
-                return false;
-        return true;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        int ans = 0;
-        List<Integer> li = new ArrayList<Integer>();
-        for (int j = 2; j < i; j++) {
-            if (isPrime(j)) {
-                ans++;
-                li.add(j);
-            }
+        int n = sc.nextInt();
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i))
+                primes.add(i);
         }
-        System.out.print(ans + "(分别是");
-        for (int j = 0; j < li.size(); j++) {
-            System.out.print(li.get(j));
-            if (j != li.size() - 1)
+        System.out.print(primes.size() + "(分别是");
+        for (int i = 0; i < primes.size(); i++) {
+            System.out.print(primes.get(i));
+            if (i != primes.size() - 1)
                 System.out.print(",");
         }
         System.out.println(")");
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 }
